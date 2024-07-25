27e1435d8fd3688224a36a133cccde17c8ca346c
BallinCat42 <jonhnevibel@gmail.com>
2024-07-25 19:49:56 +0700

    :(

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,16 @@
+import React, { useEffect, useState } from 'react';
+import hamburger from './assets/Hamburger.svg';
+import song from "./assets/Song.svg";
+import Home from './assets/Home.svg';
+import Library from './assets/Library.svg';
+import Compass from './assets/Compass.svg';
+function Sidebar() {
+  
 
-function Sidebar(){
-
-    return(<>
-
-    </>)
+  return (
+<>
+</>
+  );
 }
-export default Sidebar
+
+export default Sidebar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ export default {
   content: ["./index.html",
     "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'custom-gradient': 'linear-gradient(208deg, rgba(87,0,255,1) 1%, rgba(22,33,62,1) 65%)',
+      },
+      
+    },
   },
   plugins: [],
 }
