4f64405e7ff547432822ec94b44cdbd418cfecd5
BallinCat42 <jonhnevibel@gmail.com>
2024-07-31 18:55:54 +0700

    add the functionallity to the scrollbar

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,33 +1,43 @@
-import React, { useState } from 'react';
 import song from "./assets/icons/Song.svg";
 import Card from "./card.jsx"
 import { IoIosSearch } from "react-icons/io";
+import React, { useState, useRef } from 'react'; // Import useRef here
 
-function Searchbar(){
-  const [show, setShow] = useState(true);
-    return(
-        <>
-            <div className='flex bg-primary w-[65rem] flex-col justify-center items-center max-h-[30rem] rounded-lg mx-auto mt-10'>
-          <div className='flex flex-col w-[100%] px-12'>
-                    <span className='flex w-[60rem] mx-auto'>
-                    <input type="text" className='w-[100%] h-[3rem] rounded-md bg-primary text-slate-500 outline-none border-2 font-medium border-hover2 pl-4 text-xl ' placeholder="Search..."/>
-                    </span>
-                <div className='w-[100%] h-[23rem] flex flex-col pt-5 mx-auto'>
-                    <h2 className='text-2xl font-[500]'>
-                        Recommended Songs
-                    </h2>
-                    <div id='scrollbar' className='grid grid-flow-col overscroll-contain pb-5 max-w-[100%] overflow-x-auto gap-4 pt-4'>
-                        <Card img={``} name="Magnolia" artist="Laufey"/>
-                        <Card img={``} name="Magnolia" artist="Laufey"/>
-                        <Card img={``} name="Magnolia" artist="Laufey"/>
-                        <Card img={``} name="Magnolia" artist="Laufey"/>
-                        <Card img={``} name="Magnolia" artist="Laufey"/>
-                        <Card img={``} name="Magnolia" artist="Laufey"/>
-                    </div>
-                </div>
-                </div>
+function Searchbar() {
+  const scrollContainerRef = useRef(null);
+
+  const handleScroll = (e) => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollLeft += e.deltaY * 5;
+    }
+  };
+
+  return (
+    <>
+      <div className="flex bg-primary w-[65rem] flex-col justify-center items-center max-h-[30rem] rounded-lg mx-auto mt-10">
+        <div className="flex flex-col w-[100%] px-12">
+          <span className="flex w-[60rem] mx-auto">
+            <input
+              type="text"
+              className="w-[100%] h-[3rem] rounded-md bg-primary text-slate-500 outline-none border-2 font-medium border-hover2 pl-4 text-xl"
+              placeholder="Search..."
+            />
+          </span>
+          <div className="w-full h-[23rem] flex flex-col pt-5 mx-auto">
+            <h2 className="text-2xl font-[500]">Recommended Songs</h2>
+            <div
+              id="scrollbar"
+              ref={scrollContainerRef}
+              className="grid grid-flow-col overscroll-contain scroll-smooth pb-5 w-full overflow-x-auto gap-4 pt-4 overflow-y-hidden"
+              onWheel={handleScroll}
+            >
+              <Card />
             </div>
-        </>
-    )
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
-export default Searchbar
+
+export default Searchbar;
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,39 +1,61 @@
 import Background from 'three/examples/jsm/renderers/common/Background.js'
 import icon from './assets/icons/3dots.svg'
-function Card(props){
+import MagnoliaG from './assets/pics/Magnolia.svg'
+import GlueSongG from './assets/pics/GlueSong.svg'
+import LoversRockG from './assets/pics/LoversRock.svg'
+import ValentineG from './assets/pics/Valentine.svg'
+import MesmerizerG from './assets/pics/Mesmerizer.svg'
+import LikeTheMoviesG from './assets/pics/LikeTheMovies.svg'
+import PasilyoG from './assets/pics/Pasilyo.svg'
+import BestFriendG from './assets/pics/BestFriend.svg'
+import ThePerfectPairG from './assets/pics/ThePerfectPair.svg'
+import ForLoversG from './assets/pics/ForLovers.svg'
+import HirogaruNamidaG from './assets/pics/HirogaruNamida.svg'
+import HatachiNoKoiG from './assets/pics/HatachiNoKoi.svg'
 
-const Songs = [
-    //A default data
- {id:0,  name:'Magnolia',         artist:'Laufey',              /*img:{MagnoliaG},       mp3:{Magnolia3}*/},
- {id:1,  name:'Glue Song',        artist:'Beabadoobee',         /*img:{GlueSongG},       mp3:{GlueSong3}*/},
- {id:2,  name:'Lovers Rock',      artist:'TV Girl',             /*img:{LoversRockG},     mp3:{LoversRock3}*/},
- {id:3,  name:'Valentine',        artist:'Laufey',              /*img:{ValentineG},      mp3:{Valentine3}*/},
- {id:4,  name:'Mesmerizer',       artist:'Hatsune Miku & Teto', /*img:{MesmerizerG},     mp3:{Mesmerizer3}*/},
- {id:5,  name:'Like The Movies',  artist:'Laufey',              /*img:{LikeTheMoviesG},  mp3:{LikeTheMovies3}*/},
- {id:6,  name:'Pasilyo',          artist:'Sunkissed Lola',      /*img:{PasilyoG},        mp3:{Pasilyo3}*/},
- {id:7,  name:'Best Friend',      artist:'Laufey',              /*img:{BestFriendG},     mp3:{BestFriend3}*/},
- {id:8,  name:'The Perfect Pair', artist:'Beabadoobee',         /*img:{ThePerfectPairG}, mp3:{ThePerfectPair3}*/},
- {id:9,  name:'For Lovers',       artist:'Lamp',                /*img:{ForLoversG},      mp3:{ForLovers3}*/},
- {id:10, name:'Hirogaru-Namida',  artist:'Lamp',                /*img:{HirogaruNamidaG}, mp3:{HirogaruNamida3}*/},
- {id:11, name:'Hatachi No Koi',   artist:'Sunkissed Lola',      /*img:{HatachiNoKoiG},   mp3:{HatachiNoKoi3}*/},
-    //It will goes on!!
-]
+function Card() {
 
-    return(
-        <>
-            <div className="group flex flex-col hover:bg-hover p-4 justify-center rounded-md cursor-pointer h-[100%] duration-150"> 
-                <img src={props.img} className='bg-slate-400 w-[15rem] h-[10rem] rounded-md' alt="Album img" />
-                <div className="flex justify-between items-center gap-4 pt-3">
-                    <div>
-                        <h3 className='font-medium text-[1em] text-[#FFFFFF] duration-150 group-hover:text-secondary'>{props.name}</h3>
-                        <p className='text-[1em] text-[rgb(255,255,255,0.66)] duration-150 group-hover:text-secondary'>{props.artist}</p>
-                    </div>
-                    <span>
-                        <img src={icon} className='w-[2px]' alt="" />
-                    </span>
-                </div>
+  const Songs = [
+    { id: 0, name: 'Magnolia', artist: 'Laufey', img: MagnoliaG },
+    { id: 1, name: 'Glue Song', artist: 'Beabadoobee', img: GlueSongG },
+    { id: 2, name: 'Lovers Rock', artist: 'TV Girl', img: LoversRockG },
+    { id: 3, name: 'Valentine', artist: 'Laufey', img: ValentineG },
+    { id: 4, name: 'Mesmerizer', artist: 'Hatsune Miku & Teto', img: MesmerizerG },
+    { id: 5, name: 'Like The Movies', artist: 'Laufey', img: LikeTheMoviesG },
+    { id: 6, name: 'Pasilyo', artist: 'Sunkissed Lola', img: PasilyoG },
+    { id: 7, name: 'Best Friend', artist: 'Laufey', img: BestFriendG },
+    { id: 8, name: 'The Perfect Pair', artist: 'Beabadoobee', img: ThePerfectPairG },
+    { id: 9, name: 'For Lovers', artist: 'Lamp', img: ForLoversG },
+    { id: 10, name: 'Hirogaru-Namida', artist: 'Lamp', img: HirogaruNamidaG },
+    { id: 11, name: 'Hatachi No Koi', artist: 'Sunkissed Lola', img: HatachiNoKoiG },
+  ]
+
+  return (
+    <>
+      {Songs.map((song) => (
+        <div key={song.id} className="group flex flex-col hover:bg-hover p-4 justify-center rounded-md cursor-pointer h-[100%] duration-150"> 
+          <img 
+            src={song.img} 
+            className='bg-slate-400 object-cover group-hover:brightness-75 duration-150 min-w-[15rem] min-h-[11rem] max-h-[11rem] rounded-[5px]' 
+            alt="Album img" 
+          />
+          <div className="flex justify-between items-center gap-4 pt-3">
+            <div>
+              <h3 className='font-medium text-[1em] text-[#FFFFFF] duration-150 group-hover:text-secondary'>
+                {song.name}
+              </h3>
+              <p className='text-[1em] text-[rgb(255,255,255,0.66)] duration-150 group-hover:text-secondary'>
+                {song.artist}
+              </p>
             </div>
-        </>
-    )
+            <span>
+              <img src={icon} className='w-[2px]' alt="" />
+            </span>
+          </div>
+        </div>
+      ))}
+</>
+  )
 }
+
 export default Card
